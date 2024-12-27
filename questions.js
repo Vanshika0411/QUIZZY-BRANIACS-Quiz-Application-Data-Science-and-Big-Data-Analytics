5baa08febe586aea05145408fa6e671a51cbcd06
Vanshika Jain <jainvanshika0411@gmail.com>
2024-12-27 20:37:36 +0530

    Add files via upload
    Memory updated
    Project Title:
    Quiz Application: Data Science and Big Data Analytics
    
    Project Description:
    The Quiz Application is a web-based project designed to test users' knowledge of data science and big data analytics. It presents a series of 10 multiple-choice questions (MCQs), each worth 1 mark. At the end of the quiz, the application provides a detailed result showing the number of correct and incorrect answers.
    
    This project is an excellent opportunity to learn about interactive web development while building a useful educational tool.

---
+++ questions.js
@@ -0,0 +1,110 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Machines running LISP are also called?",
+        answer: "A. AI Workstations",
+        options: [
+            "A-: AI Workstations",
+            "B-: Regression",
+            "C-: Both A and B",
+            "D-: Time series prediction",
+        ]
+    },
+    {
+        numb: 2,
+        question: "Identify the language which is used in data science?",
+        answer: "D. Ruby ",
+        options: [
+            "A-: C++",
+            "B-: R",
+            "C-: Java",
+            "D-: Ruby",
+        ]
+    },
+    {
+        numb: 3,
+        question: "In which language is Hadoop written?",
+        answer: "B. Java",
+        options: [
+            "A-: C++",
+            "B-: Java",
+            "C-: Python",
+            "D-: All of the above",
+        ]
+    },
+    {
+        numb: 4,
+        question: "Machine learning is a subset of which of the following_____",
+        answer: "A. Artificial intelligence",
+        options: [
+            "A-: Artificial intelligence",
+            "B-: Deep learning",
+            "C-: Data learning",
+            "D-: None of the above",
+        ]
+    },
+    {
+        numb: 5,
+        question: "Total groups in which data can be characterized is?",
+        answer: "B. 2",
+        options: [
+            "A-: 1",
+            "B-: 2",
+            "C-: 3",
+            "D-: 4",
+        ]
+    },
+    {
+        numb: 6,
+        question: "Which of the following is not a part of the data science process?",
+        answer: "A. Communication building",
+        options: [
+            "A-: Communication building",
+            "B-: Operationalize",
+            "C-: Model planning",
+            "D-: Discovery",
+        ]
+    },
+    {
+        numb: 7,
+        question: "Identify the key data science skills among the following______",
+        answer: "D. All of the above",
+        options: [
+            "A-: Data visualization",
+            "B-: Machine learning",
+            "C-: Statistics",
+            "D-: All of the above",
+        ]
+    },
+    {
+        numb: 8,
+        question: "Identify whether true or false: Qubole is a big data tool______",
+        answer: "A. True",
+        options: [
+            "A-: True",
+            "B-: False",
+        ]
+    },
+    {
+        numb: 9,
+        question: "Data in___________bytes is called big data",
+        answer: "D. Peta",
+        options: [
+            "A-: Meta",
+            "B-: Giga",
+            "C-: Tera",
+            "D-: Peta",
+        ]
+    },
+    {
+        numb: 10,
+        question: "Big data analysis does the following except?",
+        answer: "B. Analyze data",
+        options: [
+            "A-: Spreads data",
+            "B-: Analyze data",
+            "C-: Organizes data",
+            "D-: Collect data",
+        ]
+    }
+];
